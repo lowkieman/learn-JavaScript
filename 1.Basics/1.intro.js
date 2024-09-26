ffc88efa4ed34ab2518c1ad1f7b4eec80477bd0d
lowkieman <lowkieman@gmail.com>
2024-09-26 12:39:58 +0300

    introduction to js

--- 1.Basics/1.intro.js
+++ 1.Basics/1.intro.js
@@ -1,1 +1,17 @@
-document.write("hello world")
+document.getElementById("light").style.display="none";
+function turnOnLights(){
+    document.getElementById("dark").style.display="none"
+    document.getElementById("light").style.display="inline-block";
+}
+
+function turnOffLights(){
+    document.getElementById("dark").style.display="inline-block"
+    document.getElementById("light").style.display="none";
+}
+if (time=1000){
+    const turnOn=setTimeout(turnOnLights,1000);
+}
+else{
+    const turnOff=setTimeout(turnOffLights,40000)
+}
+
