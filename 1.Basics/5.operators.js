22b88eba0e268dab387fc107aa9772aac146a5c7
lowkieman <lowkieman@gmail.com>
2024-09-28 22:15:22 +0300

    js operators

---
+++ 1.Basics/5.operators.js
@@ -0,0 +1,7 @@
+let x = 10; //assignment operator =
+let y = x + 20; //js addition operator
+let z = y - x; // js - operator
+// other operators include *,/ ,%,<=, >=,==,!=,**
+
+document.getElementById("demo").innerHTML=x+" "+ y + 
+" " + z
