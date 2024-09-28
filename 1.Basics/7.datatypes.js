e3452deb0bbeea693049514efaae56d51bdbb57f
lowkieman <lowkieman@gmail.com>
2024-09-28 23:12:35 +0300

    data types in js

---
+++ 1.Basics/7.datatypes.js
@@ -0,0 +1,28 @@
+// string
+const name1="wilfred";
+
+// numeber
+const age=24;
+
+//booleans
+
+let firstName = true;
+
+// Object 
+const person= {
+    firstName: "Wilfred",
+    lastName: "Doe",
+    age: 24
+}
+
+// arrays
+
+const fruits= ["apples","strawberries","mangoes","oranges"];
+
+// date object
+
+const now= new Date();
+
+document.write(name1+" "+age +" " + firstName+
+     " " + person +"" +fruits+ " "+now);
+
