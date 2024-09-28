cb625559242af06bea9058635ffc1f7e83cb6203
lowkieman <lowkieman@gmail.com>
2024-09-28 22:13:29 +0300

    js variables

---
+++ 1.Basics/3.variables.js
@@ -0,0 +1,11 @@
+var firstName = "Wilfred";
+let middleName = " Mamboleo ";
+const lastName = "Tinega";
+var fullName = firstName + middleName + lastName;
+document.getElementById("demo").innerHTML = fullName;
+
+let myname = fullName;
+document.getElementById("demo").innerHTML=myname;
+
+var fullName = "Oponde Nyakwaye";
+document.getElementById("demo").innerHTML= 20 + 30 + "20";
