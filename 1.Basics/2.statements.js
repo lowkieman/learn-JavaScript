b125884956aceaa4e716cbe6b2b3ed39b03c4a3c
lowkieman <lowkieman@gmail.com>
2024-09-28 22:11:58 +0300

    js statements

---
+++ 1.Basics/2.statements.js
@@ -0,0 +1,12 @@
+let a,b,c,d; //statement 1
+a=5; //statement 2
+b=6; //statement 3
+c=23;
+d=a + b + c;
+document.getElementById("demo").innerHTML=d;
+
+function myFunction(){
+    document.getElementById("demo").innerHTML=
+    "hello mamboleo";
+}
+myFunction();
